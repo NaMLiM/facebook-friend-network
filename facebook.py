542fe7f87a79147e5d36167a7443fca22d68fe5c
NaMLiM <anamnafiul99@gmail.com>
2021-11-08 13:33:48 +0700

    update facebook.py code

--- facebook.py
+++ facebook.py
@@ -1,4 +1,6 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import pickle
 from tqdm import tqdm
@@ -6,13 +8,15 @@ from dotenv import load_dotenv
 import os
 
 new_fb_vars = ["_6s5d", "b1v8xokw"]
-old_fb_vars = ["_4lh", "_39g5"]
+friend_xpath = "//a[@class='oajrlxb2 g5ia77u1 qu0x051f esr5mh6w e9989ue4 r7d6kgcz rq0escxv nhd2j8a9 nc684nl6 p7hjln8o kvgmc6g5 cxmmr5t8 oygrvhab hcukyx3x jb3vyjys rz4wbd8a qt6c0cv9 a8nywdso i1ao9s8h esuyzwwr f1sip0of lzcic4wl m9osqain gpro0wi8 b1v8xokw']"
 
+time_to_wait = 3  # Change this according to your pc and internet speed
 # Some utilities:
+
+
 def get_name_and_link(link):
     # Get username from mutual friends link
     # Also returns mutual friends link as sometimes the link is weird
-    global old_fb
     split = str.split(link, sep="/")
     if split[-2] == "www.facebook.com":
         # Corresponding to when user doesn't have a fb username.
@@ -23,23 +27,21 @@ def get_name_and_link(link):
         # We need to regenerate the friends list link from the user ID.
         name = str.split(split[-1], sep="=")[-1]
         link = "https://www.facebook.com/profile.php?id=" + name + "&sk=friends_mutual"
-        return [name, link] # We return the link directly
+        return [name, link]  # We return the link directly
         # because we manually generated it, we know it's ok, no more to do with it.
     else:
         # This means the username is fine.
         name = split[-2]
-    
-    if old_fb:
-        # Old fb gives link to full friends list, not just mutuals
-        return [name, link + "_mutual"]
-    else:
-        return [name, link]
+
+    return [name, link]
+
 
 load_dotenv()
 
 # Part 1: Getting list of your friends
 
-driver = Chrome(os.getenv("CHROME_DRIVER_PATH"))
+service = Service(ChromeDriverManager().install())
+driver = Chrome(service=service)
 
 driver.get("https://www.facebook.com")
 input("Press enter here when you have logged in to Facebook.")
@@ -47,13 +49,7 @@ input("Press enter here when you have logged in to Facebook.")
 
 driver.get(os.getenv("FRIENDS_LIST"))
 
-try:
-    driver.find_element_by_class_name(new_fb_vars[0])[0]
-    class_vars = new_fb_vars
-    old_fb = False
-except:
-    class_vars = old_fb_vars
-    old_fb = True
+class_vars = new_fb_vars
 
 # Load page fully...
 old_no_friends = -1
@@ -61,17 +57,23 @@ while len(driver.find_elements_by_class_name(class_vars[1])) != old_no_friends:
     old_no_friends = len(driver.find_elements_by_class_name(class_vars[1]))
     # Using space key to scroll down page, to fully load
     driver.find_elements_by_class_name(class_vars[0])[0].send_keys("     ")
-    time.sleep(3)
+    time.sleep(time_to_wait)
 # Page loaded!
-friends = driver.find_elements_by_class_name(class_vars[1])
+friends = driver.find_elements_by_xpath(friend_xpath)
+
 
 # Get list of mutual friend links, add it to friend_links dictionary
 print("Getting the list of mutual friends...")
 friend_links = {}
 for friend in tqdm(friends):
-    link_to_mutuals = friend.get_attribute("href")
-    name, link = get_name_and_link(link_to_mutuals)
-    friend_links[name] = link
+    try:
+        friend.click
+    except:
+        print("Error No Click")
+    finally:
+        link_to_mutuals = friend.get_attribute("href")
+        name, link = get_name_and_link(link_to_mutuals)
+        friend_links[name] = link
 
 # Dumping list of names with links, saving progress in case we lose connection
 with open("friend_links", "wb") as file:
@@ -91,11 +93,12 @@ for friend in tqdm(friend_links.keys()):
         old_no_friends = len(driver.find_elements_by_class_name(class_vars[1]))
         # Scroll using space key
         driver.find_elements_by_class_name(class_vars[0])[0].send_keys("     ")
-        time.sleep(3)
+        time.sleep(time_to_wait)
 
     # Grab friends
-    raw_mutuals = driver.find_elements_by_class_name(class_vars[1])
-    mutuals[friend] = [get_name_and_link(friend.get_attribute("href"))[0] for friend in raw_mutuals]
+    raw_mutuals = driver.find_elements_by_xpath(friend_xpath)
+    mutuals[friend] = [get_name_and_link(friend.get_attribute("href"))[
+        0] for friend in raw_mutuals]
 
 # Dumping names with list of mutual friends. We can disconnect from Facebook now.
 with open("mutuals", "wb") as f:
